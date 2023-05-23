83227be0465589ce4fd419bdc6fd0100f7f036d2
ItaloRLima <italolima3@aluno.uema.br>
2023-05-23 11:09:20 -0300

    Commit Provisorio

---
+++ components/control_wifi/control_wifi.c
@@ -0,0 +1,109 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+// WiFi parameters
+#define WIFI_SSID "VITALINA 4G"
+#define WIFI_PASS "Ju@321564"
+
+
+// Event group
+static EventGroupHandle_t wifi_event_group;
+const int CONNECTED_BIT = BIT0;
+
+// configure the wifi connection and start the interface
+wifi_config_t wifi_config = {
+    .sta = {
+      .ssid = WIFI_SSID,
+      .password = WIFI_PASS,
+    },
+};
+
+
+// Wifi event handler
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+  switch (event->event_id) {
+
+    case SYSTEM_EVENT_STA_START:
+      esp_wifi_connect();
+      break;
+
+    case SYSTEM_EVENT_STA_GOT_IP:
+      xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+      break;
+
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+      printf("DISCONNECTED FROM %s!\n",WIFI_SSID);
+      break;
+
+    default:
+      break;
+  }
+
+  return ESP_OK;
+}
+
+// Main task
+void vTaskWifi(void *pvParameter)
+{
+  tcpip_adapter_ip_info_t ip_info;
+
+
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+  printf("Connecting to %s\n", WIFI_SSID);
+
+  // wait for connection
+  printf("Waiting for connection to the wifi network... \n");
+  xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+  printf("Connected!\n");
+
+  // print the local IP address
+  ESP_ERROR_CHECK(tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &ip_info));
+  printf("IP Address:  %s\n", ip4addr_ntoa(&ip_info.ip));
+  printf("Subnet mask: %s\n", ip4addr_ntoa(&ip_info.netmask));
+  printf("Gateway:     %s\n", ip4addr_ntoa(&ip_info.gw));
+  printf("You can connect now to any webserver online! :-)\n");
+
+  while (true) {
+    vTaskDelay(1000 / portTICK_RATE_MS);
+  }
+}
+
+
+// Main application
+void init()
+{
+
+  printf("ESP-IDF version used: ");
+  printf(IDF_VER"\n");
+  // disable the default wifi logging
+  esp_log_level_set("wifi", ESP_LOG_NONE);
+
+  // create the event group to handle wifi events
+  wifi_event_group = xEventGroupCreate();
+
+  // initialize the tcp stack
+  tcpip_adapter_init();
+
+  // initialize the wifi event handler
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+
+  // initialize the wifi stack in STAtion mode with config in RAM
+  wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+}
--- main/main.c
+++ main/main.c
@@ -3,13 +3,18 @@
 #include "../definition.h"
 #include "../components/control_led/include/control_led.h"
 #include "../components/control_switch/include/control_switch.h"
+#include "../components/control_wifi/include/control_wifi.h"
 
 QueueHandle_t xQueueSwitch;
 
 void vTaskLed(void *pvParameters);
 void vTaskSwitch(void *pvParameters);
+void vTaskWifi(void *pvParameters);
 
 void init_queue(void){
+    ESP_ERROR_CHECK(nvs_flash_init());
+    init();
+
     xQueueSwitch = xQueueCreate(5 , sizeof(info_led) );
 }
 
@@ -18,5 +23,6 @@ void app_main(void)
     init_queue();
 
     xTaskCreatePinnedToCore( vTaskLed, "TaskLed", configMINIMAL_STACK_SIZE + 2048, NULL, 4, NULL, 0 );
+    xTaskCreatePinnedToCore( vTaskWifi, "TaskWifi", configMINIMAL_STACK_SIZE + 2048, NULL, 4, NULL, 0);
     xTaskCreatePinnedToCore( vTaskSwitch, "TaskSwitch", configMINIMAL_STACK_SIZE + 2048, NULL, 4, NULL, 1 );
 }
